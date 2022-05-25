ecf715716f21c322630d8eb7faecdcdf4c00a560
hnikoloski <h.nikoloski@outlook.com>
2022-05-25 16:05:08 +0200

    sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,10 +2,12 @@
 
 const mix = require("laravel-mix");
 
+mix.disableNotifications();
 mix
   .options({
     processCssUrls: false,
   })
-  .js("src/app.js", "js")
-  .sass("src/app.scss", "css")
-  .setPublicPath("dist");
+  .js('src/app.js', 'js')
+  .sass('src/app.scss', 'css')
+  .setPublicPath('dist')
+  .sourceMaps(true, "source-map")
